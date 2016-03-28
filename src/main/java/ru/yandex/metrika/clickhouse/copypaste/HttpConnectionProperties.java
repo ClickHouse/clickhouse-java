4e16fb7bbe19efb1c5d7b2c3e0a376f65609594c
serebrserg <serebrserg@yandex-team.ru>
2016-03-28 19:21:21 +0300

    METR-20494: add CHQueryParam enum

--- src/main/java/ru/yandex/metrika/clickhouse/copypaste/HttpConnectionProperties.java
+++ src/main/java/ru/yandex/metrika/clickhouse/copypaste/HttpConnectionProperties.java
@@ -3,6 +3,7 @@ package ru.yandex.metrika.clickhouse.copypaste;
 import java.util.Properties;
 
 import static ru.yandex.metrika.clickhouse.copypaste.CHConnectionSettings.*;
+import static ru.yandex.metrika.clickhouse.copypaste.CHQueryParam.*;
 /**
  * User: hamilkar
  * Date: 10/17/13
@@ -17,79 +18,89 @@ public class HttpConnectionProperties {
      * На стороне clickhouse сделаны ограничения на запросы.
      * https://svn.yandex.ru/websvn/wsvn/conv/trunk/metrica/src/dbms/src/Server/config.conf
      */
-    private String profile = PROFILE_SETTING_DEFAULT;
-    private boolean compress = COMPRESS_SETTING_DEFAULT;
+    private String profile;
+    private boolean compress;
     // asynchronous=0&max_threads=1
-    private boolean async = ASYNC_SETTING_DEFAULT;
-    private Integer maxThreads = MAX_THREADS_SETTING_DEFAULT;
-    private Integer maxBlockSize = MAX_BLOCK_SIZE_SETTING_DEFAULT;
+    private boolean async;
+    private Integer maxThreads;
+    private Integer maxBlockSize;
 
-    private int bufferSize = BUFFER_SIZE_SETTING_DEFAULT;
-    private int apacheBufferSize = APACHE_BUFFER_SIZE_SETTING_DEFAULT;
+    private int bufferSize;
+    private int apacheBufferSize;
 
     //настройки для демонов
-    private int socketTimeout = SOCKET_TIMEOUT_SETTING_DEFAULT;
-    private int connectionTimeout = CONNECTION_TIMEOUT_SETTING_DEFAULT;
+    private int socketTimeout;
+    private int connectionTimeout;
 
     //METR-9568: параметр user для определения профиля настроек(?).
-    private String user = USER_SETTING_DEFAULT;
+    private String user;
 
     /*
     * это таймаут на передачу данных.
     * Число socketTimeout + dataTransferTimeout отправляется в clickhouse в параметре max_execution_time
     * После чего кликхаус сам останавливает запрос если время его выполнения превышает max_execution_time
     * */
-    private int dataTransferTimeout = DATA_TRANSFER_TIMEOUT_SETTING_DEFAULT;
-    private int keepAliveTimeout = KEEP_ALIVE_TIMEOUT_SETTING_DEFAULT;
+    private int dataTransferTimeout;
+    private int keepAliveTimeout;
 
     /**
      * Для ConnectionManager'а
      */
-    private int timeToLiveMillis = TIME_TO_LIVE_MILLIS_SETTING_DEFAULT;
-    private int defaultMaxPerRoute = DEFAULT_MAX_PER_ROUTE_SETTING_DEFAULT;
-    private int maxTotal = MAX_TOTAL_SETTING_DEFAULT;
+    private int timeToLiveMillis;
+    private int defaultMaxPerRoute;
+    private int maxTotal;
 
     public HttpConnectionProperties() {
+        this(new Properties());
     }
 
     public HttpConnectionProperties(Properties info) {
-        this.profile = getSetting(info, PROFILE_SETTING, PROFILE_SETTING_DEFAULT);
-        this.compress = getSetting(info, COMPRESS_SETTING, COMPRESS_SETTING_DEFAULT);
-        this.async = getSetting(info, ASYNC_SETTING, ASYNC_SETTING_DEFAULT);
-        this.maxThreads = getSetting(info, MAX_THREADS_SETTING, MAX_THREADS_SETTING_DEFAULT);
-        this.maxBlockSize = getSetting(info, MAX_BLOCK_SIZE_SETTING, MAX_BLOCK_SIZE_SETTING_DEFAULT);
+        this.profile = getSetting(info, PROFILE);
+        this.compress = getSetting(info, COMPRESS);
+        this.async = getSetting(info, ASYNC);
+        this.maxThreads = getSetting(info, MAX_THREADS);
+        this.maxBlockSize = getSetting(info, MAX_BLOCK_SIZE);
 
-        this.bufferSize = getSetting(info, BUFFER_SIZE_SETTING, BUFFER_SIZE_SETTING_DEFAULT);
-        this.apacheBufferSize = getSetting(info, APACHE_BUFFER_SIZE_SETTING,APACHE_BUFFER_SIZE_SETTING_DEFAULT);
+        this.bufferSize = getSetting(info, BUFFER_SIZE);
+        this.apacheBufferSize = getSetting(info, APACHE_BUFFER_SIZE);
 
-        this.socketTimeout = getSetting(info, SOCKET_TIMEOUT_SETTING, SOCKET_TIMEOUT_SETTING_DEFAULT);
-        this.connectionTimeout = getSetting(info, CONNECTION_TIMEOUT_SETTING, CONNECTION_TIMEOUT_SETTING_DEFAULT);
+        this.socketTimeout = getSetting(info, SOCKET_TIMEOUT);
+        this.connectionTimeout = getSetting(info, CONNECTION_TIMEOUT);
 
-        this.user = getSetting(info, USER_SETTING, USER_SETTING_DEFAULT);
+        this.user = getSetting(info, USER);
 
-        this.dataTransferTimeout = getSetting(info, DATA_TRANSFER_TIMEOUT_SETTING, DATA_TRANSFER_TIMEOUT_SETTING_DEFAULT);
-        this.keepAliveTimeout = getSetting(info, KEEP_ALIVE_TIMEOUT_SETTING, KEEP_ALIVE_TIMEOUT_SETTING_DEFAULT);
+        this.dataTransferTimeout = getSetting(info, DATA_TRANSFER_TIMEOUT);
+        this.keepAliveTimeout = getSetting(info, KEEP_ALIVE_TIMEOUT);
 
-        this.timeToLiveMillis = getSetting(info, TIME_TO_LIVE_MILLIS_SETTING, TIME_TO_LIVE_MILLIS_SETTING_DEFAULT);
-        this.defaultMaxPerRoute = getSetting(info, DEFAULT_MAX_PER_ROUTE_SETTING, DEFAULT_MAX_PER_ROUTE_SETTING_DEFAULT);
-        this.maxTotal = getSetting(info, MAX_TOTAL_SETTING, MAX_TOTAL_SETTING_DEFAULT);
+        this.timeToLiveMillis = getSetting(info, TIME_TO_LIVE_MILLIS);
+        this.defaultMaxPerRoute = getSetting(info, DEFAULT_MAX_PER_ROUTE);
+        this.maxTotal = getSetting(info, MAX_TOTAL);
+    }
+
+
+    private <T> T getSetting(Properties info, CHQueryParam param){
+        return getSetting(info, param.getKey(), param.getDefaultValue(), param.getClazz());
+    }
+
+    private <T> T getSetting(Properties info, CHConnectionSettings settings){
+        return getSetting(info, settings.getKey(), settings.getDefaultValue(), settings.getClazz());
     }
 
     @SuppressWarnings("unchecked")
-    private <T> T getSetting(Properties info, String key, T defaultValue){
+    private <T> T getSetting(Properties info, String key, Object defaultValue, Class clazz){
         Object val = info.get(key);
         if (val == null)
-            return defaultValue;
-        if ((defaultValue.getClass() == int.class || defaultValue.getClass() == Integer.class) && val instanceof String) {
-            return (T) defaultValue.getClass().cast(Integer.valueOf((String) val));
+            return (T)defaultValue;
+        if ((clazz == int.class || clazz == Integer.class) && val instanceof String) {
+            return (T) clazz.cast(Integer.valueOf((String) val));
         }
-        if ((defaultValue.getClass() == long.class || defaultValue.getClass() == Long.class) && val instanceof String) {
-            return (T) defaultValue.getClass().cast(Long.valueOf((String) val));
+        if ((clazz == long.class || clazz == Long.class) && val instanceof String) {
+            return (T) clazz.cast(Long.valueOf((String) val));
         }
-        if ((defaultValue.getClass() == boolean.class || defaultValue.getClass() == Boolean.class) && val instanceof String) {
-            return (T) defaultValue.getClass().cast(Boolean.valueOf((String) val));
+        if ((clazz == boolean.class || clazz == Boolean.class) && val instanceof String) {
+            return (T) clazz.cast(Boolean.valueOf((String) val));
         }
-        return (T) defaultValue.getClass().cast(val);
+        return (T) clazz.cast(val);
     }
 
 
