d4f1837a535913a1cfc3473a36ce56147f84f62c
serebrserg <serebrserg@yandex-team.ru>
2016-04-21 17:30:38 +0300

    METR-21015: add all CHErrorCodes from CH. append exception message from clickhouse to CHException message

--- src/main/java/ru/yandex/metrika/clickhouse/except/CHException.java
+++ src/main/java/ru/yandex/metrika/clickhouse/except/CHException.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 public class CHException extends SQLException {
 
     public CHException(int code, Throwable cause, String host, int port) {
-        super("ClickHouse exception, code: " + code + ", host: " + host + ", port: " + port, null, code, cause);
+        super("ClickHouse exception, code: " + code + ", host: " + host + ", port: " + port + "; " + cause.getMessage(), null, code, cause);
     }
 
     public CHException(int code, String message, Throwable cause, String host, int port) {
-        super("ClickHouse exception, message: " + message + ", host: " + host + ", port: " + port, null, code, cause);
+        super("ClickHouse exception, message: " + message + ", host: " + host + ", port: " + port + "; " + cause.getMessage(), null, code, cause);
     }
 }
