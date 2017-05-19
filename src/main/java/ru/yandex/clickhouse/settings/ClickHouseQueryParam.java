20f5332d2c11143e6e36364b8e7db1797ab2edfd
timofeevb <timofeevb@yandex-team.ru>
2017-05-19 18:54:21 +0300

    Format fixed

--- src/main/java/ru/yandex/clickhouse/settings/ClickHouseQueryParam.java
+++ src/main/java/ru/yandex/clickhouse/settings/ClickHouseQueryParam.java
@@ -220,7 +220,7 @@ public enum ClickHouseQueryParam implements DriverPropertyCreator {
 
     USER("user", null, String.class, "user name, by default - default"),
 
-    PREFERRED_BLOCK_SIZE_BYTES("preferred_block_size_bytes", null, Long.class, "");
+    PREFERRED_BLOCK_SIZE_BYTES("preferred_block_size_bytes", null, Long.class, ""),;
 
     private final String key;
     private final Object defaultValue;
