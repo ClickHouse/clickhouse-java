0b74f2b83a0a2796e5a4fa5e1dfec0a6f7763d74
Jolene Wong <jolwong@opendns.com>
2018-03-14 12:54:35 -0700

    array of array parsing

--- src/main/java/ru/yandex/clickhouse/util/ClickHouseArrayUtil.java
+++ src/main/java/ru/yandex/clickhouse/util/ClickHouseArrayUtil.java
@@ -102,6 +102,15 @@ public class ClickHouseArrayUtil {
 
 
     public static String toString(Object[] values) {
+        if (values.length > 0 && (values[0].getClass().isArray() || values[0] instanceof Collection)) {
+            // quote is false to avoid escaping inner '['
+            ArrayBuilder builder = new ArrayBuilder(false);
+            for (Object value : values) {
+                Object[] objects = ((Collection) value).toArray();
+                builder.append(toString(objects));
+            }
+            return builder.build();
+        }
         ArrayBuilder builder = new ArrayBuilder(needQuote(values));
         for (Object value : values) {
             builder.append(value);
