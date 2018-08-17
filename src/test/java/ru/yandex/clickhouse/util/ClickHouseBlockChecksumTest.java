d5eff7f591c84ca09e300da4f40f56a60cfbccc5
ezhaka <cookies.moonster@gmail.com>
2018-08-17 12:29:20 +0300

    Fix checksum calculation again (#225)

--- src/test/java/ru/yandex/clickhouse/util/ClickHouseBlockChecksumTest.java
+++ src/test/java/ru/yandex/clickhouse/util/ClickHouseBlockChecksumTest.java
@@ -14,7 +14,7 @@ public class ClickHouseBlockChecksumTest {
     private static final int HEADER_SIZE_BYTES = 9;
 
     @Test
-    public void calculateChecksum() {
+    public void trickyBlock() {
         byte[] compressedData = DatatypeConverter.parseHexBinary("1F000100078078000000B4000000");
         int uncompressedSizeBytes = 35;
 
@@ -27,8 +27,27 @@ public class ClickHouseBlockChecksumTest {
         );
 
         Assert.assertEquals(
-            DatatypeConverter.printHexBinary(checksum.asBytes()),
-            "923AF569343D26F98EAEF00EEFEC36A9"
+            new ClickHouseBlockChecksum(-493639813825217902L, -6253550521065361778L),
+            checksum
+        );
+    }
+
+    @Test
+    public void anotherTrickyBlock() {
+        byte[] compressedData = DatatypeConverter.parseHexBinary("80D9CEF753E3A59B3F");
+        int uncompressedSizeBytes = 8;
+
+        ClickHouseBlockChecksum checksum = ClickHouseBlockChecksum.calculateForBlock(
+            (byte) ClickHouseLZ4Stream.MAGIC,
+            compressedData.length + HEADER_SIZE_BYTES,
+            uncompressedSizeBytes,
+            compressedData,
+            compressedData.length
+        );
+
+        Assert.assertEquals(
+            new ClickHouseBlockChecksum(-7135037831041210418L, -8214889029657590490L),
+            checksum
         );
     }
 }
