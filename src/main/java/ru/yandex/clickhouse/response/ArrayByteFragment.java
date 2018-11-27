14d4bee6600b466803b83ec4ce223cdb9dbf67db
Oleg Osovitskiy <olezha@yandex-team.ru>
2018-11-27 17:29:44 +0300

    support reading NULL array elements

---
+++ src/main/java/ru/yandex/clickhouse/response/ArrayByteFragment.java
@@ -0,0 +1,23 @@
+package ru.yandex.clickhouse.response;
+
+class ArrayByteFragment extends ByteFragment {
+
+    private ArrayByteFragment(byte[] buf, int start, int len) {
+        super(buf, start, len);
+    }
+
+    static ArrayByteFragment wrap(ByteFragment fragment) {
+        return new ArrayByteFragment(fragment.buf, fragment.start, fragment.len);
+    }
+
+    @Override
+    public boolean isNull() {
+        // NULL
+        return len == 4 && buf[start] == 'N' && buf[start + 1] == 'U' && buf[start + 2] == 'L' && buf[start + 3] == 'L';
+    }
+
+    public boolean isNaN() {
+        // nan
+        return len == 3 && buf[start] == 'n' && buf[start + 1] == 'a' && buf[start + 2] == 'n';
+    }
+}
