536a9afe8952219e18a3964453c94f43cf69c9e8
Felix Müller <5407006+enqueue@users.noreply.github.com>
2021-01-08 07:00:53 +0100

    Re-use comment value

--- src/main/java/ru/yandex/clickhouse/ClickHouseDatabaseMetadata.java
+++ src/main/java/ru/yandex/clickhouse/ClickHouseDatabaseMetadata.java
@@ -717,7 +717,7 @@ public class ClickHouseDatabaseMetadata implements DatabaseMetaData {
 
         List typeList = types != null ? Arrays.asList(types) : null;
         while (result.next()) {
-            List<String> row = new ArrayList<String>();
+            List<String> row = new ArrayList<>();
             row.add(DEFAULT_CAT);
             row.add(result.getString(1));
             row.add(result.getString(2));
@@ -809,7 +809,7 @@ public class ClickHouseDatabaseMetadata implements DatabaseMetaData {
                 "SELECT database, table, name, type, default_type, default_expression, NULL AS comment ");
         }
         query.append("FROM system.columns ");
-        List<String> predicates = new ArrayList<String>();
+        List<String> predicates = new ArrayList<>();
         if (schemaPattern != null) {
             predicates.add("database LIKE '" + schemaPattern + "' ");
         }
@@ -827,7 +827,7 @@ public class ClickHouseDatabaseMetadata implements DatabaseMetaData {
         ResultSet descTable = request(query.toString());
         int colNum = 1;
         while (descTable.next()) {
-            List<String> row = new ArrayList<String>();
+            List<String> row = new ArrayList<>();
             //catalog name
             row.add(DEFAULT_CAT);
             //database name
@@ -856,9 +856,10 @@ public class ClickHouseDatabaseMetadata implements DatabaseMetaData {
                 ? String.valueOf(columnNullable)
                 : String.valueOf(columnNoNulls));
             //remarks
-            row.add("".equals(descTable.getString("comment"))
+            String remarks = descTable.getString("comment");
+            row.add("".equals(remarks)
                 ? null
-                : descTable.getString("comment"));
+                : remarks);
 
             // COLUMN_DEF
             if ("DEFAULT".equals(descTable.getString("default_type"))) {
@@ -1324,10 +1325,12 @@ public class ClickHouseDatabaseMetadata implements DatabaseMetaData {
         return iface.isAssignableFrom(getClass());
     }
 
+    @Override
     public ResultSet getPseudoColumns(String catalog, String schemaPattern, String tableNamePattern, String columnNamePattern) throws SQLException {
         return null;
     }
 
+    @Override
     public boolean generatedKeyAlwaysReturned() throws SQLException {
         return false;
     }
