e44a78c4b576002078f4a33b1d9d15772f276651
bgranvea <boris@granveaud.com>
2021-02-01 15:26:19 +0100

    use TestContainers

--- src/test/java/ru/yandex/clickhouse/integration/ResultSummaryTest.java
+++ src/test/java/ru/yandex/clickhouse/integration/ResultSummaryTest.java
@@ -3,13 +3,10 @@ package ru.yandex.clickhouse.integration;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import ru.yandex.clickhouse.ClickHouseConnection;
-import ru.yandex.clickhouse.ClickHouseDataSource;
-import ru.yandex.clickhouse.ClickHousePreparedStatement;
-import ru.yandex.clickhouse.ClickHouseStatement;
-import ru.yandex.clickhouse.settings.ClickHouseProperties;
+import ru.yandex.clickhouse.*;
 import ru.yandex.clickhouse.settings.ClickHouseQueryParam;
 
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,22 +15,18 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
 import static org.testng.AssertJUnit.assertTrue;
 
-public class ResultSummary {
+public class ResultSummaryTest {
     private ClickHouseConnection connection;
 
     @BeforeTest
     public void setUp() throws Exception {
-        ClickHouseProperties properties = new ClickHouseProperties();
-        ClickHouseDataSource dataSource = new ClickHouseDataSource("jdbc:clickhouse://localhost:8123", properties);
-        connection = dataSource.getConnection();
+        connection = ClickHouseContainerForTest.newDataSource().getConnection();
         connection.createStatement().execute("CREATE DATABASE IF NOT EXISTS test");
-        connection.createStatement().execute("DROP TABLE IF EXISTS test.insert_test");
-        connection.createStatement().execute("CREATE TABLE IF NOT EXISTS test.insert_test (value UInt32) ENGINE = TinyLog");
     }
 
     @AfterTest
     public void tearDown() throws Exception {
-        connection.createStatement().execute("DROP DATABASE test");
+        connection.createStatement().execute("DROP DATABASE IF EXISTS test");
     }
 
     @Test
@@ -81,6 +74,8 @@ public class ResultSummary {
 
     @Test
     public void insertSingle() throws Exception {
+        createInsertTestTable();
+
         ClickHousePreparedStatement ps = (ClickHousePreparedStatement) connection.prepareStatement("INSERT INTO test.insert_test VALUES(?)");
         ps.setLong(1, 1);
         ps.executeQuery(Collections.singletonMap(ClickHouseQueryParam.SEND_PROGRESS_IN_HTTP_HEADERS, "true"));
@@ -91,6 +86,8 @@ public class ResultSummary {
 
     @Test
     public void insertBatch() throws Exception {
+        createInsertTestTable();
+
         ClickHousePreparedStatement ps = (ClickHousePreparedStatement) connection.prepareStatement("INSERT INTO test.insert_test VALUES(?)");
         for (long i = 0; i < 10; i++) {
             ps.setLong(1, i);
@@ -104,6 +101,8 @@ public class ResultSummary {
 
     @Test
     public void insertSelect() throws Exception {
+        createInsertTestTable();
+
         ClickHousePreparedStatement ps = (ClickHousePreparedStatement) connection.prepareStatement("INSERT INTO test.insert_test SELECT number FROM numbers(10)");
         ps.executeQuery(Collections.singletonMap(ClickHouseQueryParam.SEND_PROGRESS_IN_HTTP_HEADERS, "true"));
 
@@ -113,6 +112,8 @@ public class ResultSummary {
 
     @Test
     public void insertLargeSelect() throws Exception {
+        createInsertTestTable();
+
         ClickHousePreparedStatement ps = (ClickHousePreparedStatement) connection.prepareStatement("INSERT INTO test.insert_test SELECT number FROM numbers(10000000)");
         ps.executeQuery(Collections.singletonMap(ClickHouseQueryParam.SEND_PROGRESS_IN_HTTP_HEADERS, "true"));
 
@@ -126,5 +127,10 @@ public class ResultSummary {
         st.executeQuery("SELECT * FROM numbers(10)");
 
         assertNull(st.getResponseSummary());
+    }
+
+    private void createInsertTestTable() throws SQLException {
+        connection.createStatement().execute("DROP TABLE IF EXISTS test.insert_test");
+        connection.createStatement().execute("CREATE TABLE IF NOT EXISTS test.insert_test (value UInt32) ENGINE = TinyLog");
     }
 }
