e385c254b0261f62c9befe4735fd1e555351e0e6
Zhichun Wu <zhicwu@gmail.com>
2021-02-23 22:17:42 +0800

    Add benchmark

---
+++ clickhouse-benchmark/src/main/java/tech/clickhouse/benchmark/Constants.java
@@ -0,0 +1,18 @@
+package tech.clickhouse.benchmark;
+
+public interface Constants {
+    public static final String CLICKHOUSE_DRIVER = "clickhouse-jdbc";
+
+    public static final String DEFAULT_HOST = "127.0.0.1";
+    public static final String DEFAULT_DB = "system";
+    public static final String DEFAULT_USER = "default";
+    public static final String DEFAULT_PASSWD = "";
+
+    public static final int GRPC_PORT = 9100;
+    public static final int HTTP_PORT = 8123;
+    public static final int MYSQL_PORT = 3307;
+    public static final int NATIVE_PORT = 9000;
+
+    public static final String NORMAL_STATEMENT = "normal";
+    public static final String PREPARED_STATEMENT = "prepared";
+}
