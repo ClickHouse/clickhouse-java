827e7c189b08c881762807bc597c4a8a7e166d74
Zhichun Wu <zhicwu@gmail.com>
2021-05-06 10:05:08 +0800

    Ignore database only for DDL containing DATABASE keyword, and allow to change current database of a statement

--- clickhouse-jdbc/src/test/java/ru/yandex/clickhouse/integration/ClickHouseConnectionImplTest.java
+++ clickhouse-jdbc/src/test/java/ru/yandex/clickhouse/integration/ClickHouseConnectionImplTest.java
@@ -13,8 +13,8 @@ import ru.yandex.clickhouse.ClickHouseDataSource;
 import ru.yandex.clickhouse.except.ClickHouseException;
 import ru.yandex.clickhouse.settings.ClickHouseProperties;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
@@ -73,6 +73,54 @@ public class ClickHouseConnectionImplTest {
     @Test
     public void testOofWrongPassword() throws Exception {
         assertFailure(createDataSource("oof", "baz"));
+    }
+
+    @Test
+    public void testDefaultDatabase() throws Exception {
+        ClickHouseDataSource ds = ClickHouseContainerForTest.newDataSource();
+        String currentDbQuery = "select currentDatabase()";
+        try (Connection conn = ds.getConnection(); Statement s = conn.createStatement()) {
+            try (ResultSet rs = s.executeQuery(currentDbQuery)) {
+                assertTrue(rs.next());
+                assertEquals(rs.getString(1), "default");
+                assertFalse(rs.next());
+            }
+            s.execute("create database if not exists tdb1; create database if not exists tdb2");
+        }
+
+        ds = ClickHouseContainerForTest.newDataSource("tdb2");
+        try (Connection conn = ds.getConnection(); Statement s = conn.createStatement()) {
+            try (ResultSet rs = s.executeQuery("select currentDatabase()")) {
+                assertTrue(rs.next());
+                assertEquals(rs.getString(1), "tdb2");
+                assertFalse(rs.next());
+            }
+
+            s.execute("create table tdb2_aaa(a String) engine=Memory; insert into tdb2_aaa values('3')");
+
+            try (ResultSet rs = s.executeQuery("select currentDatabase(), a from tdb2_aaa")) {
+                assertTrue(rs.next());
+                assertEquals(rs.getString(1), "tdb2");
+                assertEquals(rs.getString(2), "3");
+                assertFalse(rs.next());
+            }
+
+            s.execute("use tdb1; create table tdb1_aaa(a String) engine=Memory; insert into tdb1_aaa values('1')");
+
+            try (ResultSet rs = s.executeQuery("select currentDatabase(), a from tdb1_aaa")) {
+                assertTrue(rs.next());
+                assertEquals(rs.getString(1), "tdb1");
+                assertEquals(rs.getString(2), "1");
+                assertFalse(rs.next());
+            }
+
+            try (ResultSet rs = s.executeQuery("use `tdb2`; select currentDatabase(), a from tdb2_aaa")) {
+                assertTrue(rs.next());
+                assertEquals(rs.getString(1), "tdb2");
+                assertEquals(rs.getString(2), "3");
+                assertFalse(rs.next());
+            }
+        }
     }
 
     private static void assertSuccess(DataSource dataSource) throws Exception {
