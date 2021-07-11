e55b88ac96f4b27248a8d4327468e0c9cf60c810
Zhichun Wu <zhicwu@gmail.com>
2021-07-12 06:45:19 +0800

    Update benchmarks

--- clickhouse-benchmark/src/main/java/tech/clickhouse/benchmark/Basic.java
+++ clickhouse-benchmark/src/main/java/tech/clickhouse/benchmark/Basic.java
@@ -3,32 +3,27 @@ package tech.clickhouse.benchmark;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Collections;
-import java.util.Random;
 import org.openjdk.jmh.annotations.Benchmark;
 
 public class Basic extends JdbcBenchmark {
     @Benchmark
+    public int insertOneRandomNumber(ClientState state) throws Throwable {
+        final int num = state.getRandomSample();
+
+        return executeInsert(state, "insert into test_insert(i) values(?)",
+                Collections.enumeration(Collections.singletonList(new Object[] { num })));
+    }
+
+    @Benchmark
     public int selectOneRandomNumber(ClientState state) throws Throwable {
-        final int num = new Random().nextInt(1000);
+        final int num = state.getRandomSample();
 
-        try (Statement stmt = executeQuery(state, "select ? as n", num)) {
-            ResultSet rs = stmt.getResultSet();
-
-            rs.next();
-
-            if (num != rs.getInt(1)) {
+        try (Statement stmt = executeQuery(state, "select ? as n", num); ResultSet rs = stmt.getResultSet();) {
+            if (!rs.next() || num != rs.getInt(1)) {
                 throw new IllegalStateException();
             }
 
             return num;
         }
-    }
-
-    @Benchmark
-    public int insertOneRandomNumber(ClientState state) throws Throwable {
-        final int num = new Random().nextInt(1000);
-
-        return executeInsert(state, "insert into test_insert(i) values(?)",
-                Collections.enumeration(Collections.singletonList(new Object[] { num })));
     }
 }
