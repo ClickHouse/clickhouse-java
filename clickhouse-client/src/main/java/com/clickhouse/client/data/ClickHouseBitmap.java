987cf14cf126e93626279b5edb7af19080b4275e
遛猫达人 <m18280054202@163.com>
2022-08-03 10:53:17 +0800

    https://github.com/RoaringBitmap/RoaringBitmap/issues/366

--- clickhouse-client/src/main/java/com/clickhouse/client/data/ClickHouseBitmap.java
+++ clickhouse-client/src/main/java/com/clickhouse/client/data/ClickHouseBitmap.java
@@ -1,16 +1,16 @@
 package com.clickhouse.client.data;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Objects;
+import java.util.*;
+
+import org.roaringbitmap.BitmapDataProvider;
 import org.roaringbitmap.RoaringBitmap;
 import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
 import org.roaringbitmap.buffer.MutableRoaringBitmap;
@@ -171,9 +171,32 @@ public abstract class ClickHouseBitmap {
                 DataOutput out = new DataOutputStream(bas);
                 try {
                     // https://github.com/RoaringBitmap/RoaringBitmap/blob/0.9.9/RoaringBitmap/src/main/java/org/roaringbitmap/longlong/Roaring64NavigableMap.java#L1105
-                    rb.serialize(out);
+                    Field signedLongsField = Roaring64NavigableMap.class.getDeclaredField("signedLongs");
+                    Field highToBitmapField = Roaring64NavigableMap.class.getDeclaredField("highToBitmap");
+                    AccessibleObject.setAccessible(new Field[]{signedLongsField, highToBitmapField}, true);
+                    boolean signedLongs = signedLongsField.getBoolean(rb);
+                    NavigableMap<Integer, BitmapDataProvider> highToBitmap =
+                            (NavigableMap<Integer, BitmapDataProvider>) highToBitmapField.get(rb);
+                    out.writeBoolean(signedLongs);
+                    out.writeInt(highToBitmap.size());
+                    Iterator var2 = highToBitmap.entrySet().iterator();
+
+                    while(var2.hasNext()) {
+                        Map.Entry<Integer, BitmapDataProvider> entry = (Map.Entry)var2.next();
+                        //here is the different between the implements of java and c
+                        Integer v = entry.getKey();
+                        out.write((byte)(v >>>  0));
+                        out.write((byte)(v >>>  8));
+                        out.write((byte)(v >>>  16));
+                        out.write((byte)(v >>>  24));
+                        ((BitmapDataProvider)entry.getValue()).serialize(out);
+                    }
                 } catch (IOException e) {
                     throw new IllegalArgumentException("Failed to serialize given bitmap", e);
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
                 }
 
                 byte[] bytes = bas.toByteArray();
@@ -374,8 +397,8 @@ public abstract class ClickHouseBitmap {
                 }
                 // read the rest
                 in.readFully(bytes, 5, len - 8);
-                Roaring64NavigableMap b = new Roaring64NavigableMap();
-                b.deserialize(new DataInputStream(new ByteArrayInputStream(bytes)));
+                Roaring64NavigableMap b = deserializeFromCRoaring(new DataInputStream(new ByteArrayInputStream(bytes)));
+
                 rb = ClickHouseBitmap.wrap(b, innerType);
             }
         }
@@ -453,12 +476,74 @@ public abstract class ClickHouseBitmap {
                 ((Buffer) buffer).position(buffer.position() - 5);
                 bitmaps = new byte[buffer.remaining()];
                 buffer.get(bitmaps);
-                Roaring64NavigableMap b = new Roaring64NavigableMap();
-                b.deserialize(new DataInputStream(new ByteArrayInputStream(bitmaps)));
+                Roaring64NavigableMap b = deserializeFromCRoaring(new DataInputStream(new ByteArrayInputStream(bitmaps)));
                 rb = ClickHouseBitmap.wrap(b, innerType);
             }
         }
 
+        return rb;
+    }
+
+    public static Roaring64NavigableMap deserializeFromCRoaring(DataInput in) throws IOException {
+        Roaring64NavigableMap rb = new Roaring64NavigableMap();
+        try {
+            rb.clear();
+            Field signedLongsField = Roaring64NavigableMap.class.getDeclaredField("signedLongs");
+            Field highToBitmapField = Roaring64NavigableMap.class.getDeclaredField("highToBitmap");
+            Method newRoaringBitmapMethod = Roaring64NavigableMap.class.getDeclaredMethod("newRoaringBitmap");
+            Method resetPerfHelpersMethod = Roaring64NavigableMap.class.getDeclaredMethod("resetPerfHelpers");
+            Class roaringIntPackingClass = Class.forName("org.roaringbitmap.longlong.RoaringIntPacking");
+            Method unsignedComparatorMethod = roaringIntPackingClass.getDeclaredMethod("unsignedComparator");
+            AccessibleObject.setAccessible(new AccessibleObject[]{
+                    signedLongsField,unsignedComparatorMethod,
+                    highToBitmapField,
+                    newRoaringBitmapMethod, resetPerfHelpersMethod}, true);
+
+            NavigableMap<Integer, BitmapDataProvider> highToBitmap = null;
+            boolean signedLongs = in.readBoolean();
+
+            signedLongsField.setBoolean(rb, signedLongs);
+
+            int nbHighs = in.readInt();
+            if (signedLongs) {
+                highToBitmap = new TreeMap();
+            } else {
+                highToBitmap = new TreeMap((Comparator) unsignedComparatorMethod.invoke(null));
+            }
+            highToBitmapField.set(rb, highToBitmap);
+            for (int i = 0; i < nbHighs; ++i) {
+                byte[] bytes = new byte[4];
+                in.readFully(bytes);
+                if ((bytes[0] | bytes[1] | bytes[2] | bytes[3]) < 0)
+                    throw new EOFException();
+                int high = ((bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + (bytes[0] << 0));
+                BitmapDataProvider provider = (BitmapDataProvider) newRoaringBitmapMethod.invoke(rb);
+
+                if (provider instanceof RoaringBitmap) {
+                    ((RoaringBitmap) provider).deserialize(in);
+                } else {
+                    if (!(provider instanceof MutableRoaringBitmap)) {
+                        throw new UnsupportedEncodingException("Cannot deserialize a " + provider.getClass());
+                    }
+
+                    ((MutableRoaringBitmap) provider).deserialize(in);
+                }
+
+                highToBitmap.put(high, provider);
+            }
+            resetPerfHelpersMethod.invoke(rb);
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return rb;
     }
 
