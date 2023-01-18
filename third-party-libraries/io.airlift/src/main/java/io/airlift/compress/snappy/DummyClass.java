68565ae164e48ed823b1fbe71acdeec67b9221b7
Zhichun Wu <zhicwu@gmail.com>
2023-01-18 14:47:40 +0800

    bump 3rd party libs

---
+++ third-party-libraries/io.airlift/src/main/java/io/airlift/compress/snappy/DummyClass.java
@@ -0,0 +1,12 @@
+package io.airlift.compress.snappy;
+
+/**
+ * Dummy class as placeholder. It will be removed during package phase.
+ */
+public class DummyClass {
+    /**
+     * Default constructor.
+     */
+    public DummyClass() {
+    }
+}
---
+++ third-party-libraries/io.airlift/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module io.airlift.aircompressor {
+    exports io.airlift.compress;
+    
+    exports io.airlift.compress.gzip;
+    exports io.airlift.compress.lz4;
+    exports io.airlift.compress.lzo;
+    exports io.airlift.compress.snappy;
+    exports io.airlift.compress.zstd;
+}
