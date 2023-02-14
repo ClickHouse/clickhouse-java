5d22f371b219d29d5e4224e5ff934534c51e7e34
Alexey Milovidov <milovidov@yandex-team.ru>
2023-02-14 14:44:22 +0300

    Update ClickHouseCityHash.java

--- clickhouse-data/src/main/java/com/clickhouse/data/ClickHouseCityHash.java
+++ clickhouse-data/src/main/java/com/clickhouse/data/ClickHouseCityHash.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 YANDEX LLC
+ * Copyright 2023 ClickHouse, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
