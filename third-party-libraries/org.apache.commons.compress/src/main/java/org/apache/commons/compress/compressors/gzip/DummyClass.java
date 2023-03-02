ec8c62273c21da37f49d7c5b048be154cf9e401b
Zhichun Wu <zhicwu@gmail.com>
2023-03-02 11:07:18 +0800

    Add alias of commons-compress and bump version

---
+++ third-party-libraries/org.apache.commons.compress/src/main/java/org/apache/commons/compress/compressors/gzip/DummyClass.java
@@ -0,0 +1,12 @@
+package org.apache.commons.compress.compressors.gzip;
+
+/**
+ * Dummy class for generating sources and javadoc jars.
+ */
+public class DummyClass {
+    /**
+     * Default constructor.
+     */
+    public DummyClass() {
+    }
+}
