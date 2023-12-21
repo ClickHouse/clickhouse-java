804bea72be5135827f7ed0e12fedeb0cf0563fb6
mzitnik <mark.zitnik@clickhouse.com>
2023-12-21 21:50:11 +0200

    Adding another test for decompress issue.

--- clickhouse-jdbc/src/test/java/com/clickhouse/jdbc/JdbcIssuesTest.java
+++ clickhouse-jdbc/src/test/java/com/clickhouse/jdbc/JdbcIssuesTest.java
@@ -87,4 +87,41 @@ public class JdbcIssuesTest extends JdbcIntegrationTest {
         }
         Assert.assertFalse(failed, String.format("Failed when content size %d", count));
     }
+    @Test
+    public void test03Decompress() throws SQLException {
+        String httpEndpoint = "http://" + getServerAddress(ClickHouseProtocol.HTTP) + "/";
+        String TABLE_NAME = "decompress_issue_02";
+        Properties prop = new Properties();
+        prop.setProperty("decompress", "true");
+        prop.setProperty("decompress_algorithm", "lz4");
+        String url = String.format("jdbc:ch:%s", httpEndpoint);
+        ClickHouseDataSource dataSource = new ClickHouseDataSource(url, prop);
+        String columnNames = "event_id, num01,event_id_01 ";
+        String columnValues = "('event_id String, num01 Int8, event_id_01 String')";
+        String sql = String.format("INSERT INTO %s (%s) SELECT %s FROM input %s", TABLE_NAME, columnNames, columnNames, columnValues);
+
+        Connection conn = dataSource.getConnection("default", "");
+        Statement st = conn.createStatement();
+        st.execute(String.format("CREATE TABLE %s (`event_id` String, `num01` Int8, `event_id_01` String) ENGINE = Log", TABLE_NAME));
+
+        int count = 1;
+        boolean failed = false;
+
+        String content = StringUtils.repeat("*", 50000);
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            while (count <= 3) {
+                ps.setString(1, content);
+                ps.setInt(2, 10);
+                ps.setString(3, content);
+                ps.addBatch();
+                count += 1;
+            }
+            ps.executeBatch();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            failed = true;
+        }
+        Assert.assertFalse(failed, String.format("Failed when content size %d", count));
+    }
+
 }
