bae73489cb6823c1c27619bb28504309f93f7a0f
Maciej Maciejko <maciej.maciejko@getindata.com>
2024-01-24 17:40:43 +0100

    delegation support

--- clickhouse-http-client/src/main/java/com/clickhouse/client/gss/GssAuthorizator.java
+++ clickhouse-http-client/src/main/java/com/clickhouse/client/gss/GssAuthorizator.java
@@ -1,5 +1,11 @@
 package com.clickhouse.client.gss;
 
+import java.io.File;
+import java.security.AccessController;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+
 import org.apache.hc.client5.http.utils.Base64;
 import org.ietf.jgss.GSSContext;
 import org.ietf.jgss.GSSCredential;
@@ -21,12 +27,24 @@ public class GssAuthorizator {
     }
 
     public String getAuthToken() throws GSSException {
+        GSSCredential gssCredential = null;
+        Subject sub = Subject.getSubject(AccessController.getContext());
+        if (sub != null) {
+            Set<GSSCredential> gssCreds = sub.getPrivateCredentials(GSSCredential.class);
+            if (gssCreds != null && !gssCreds.isEmpty()) {
+                gssCredential = gssCreds.iterator().next();
+            }
+        }
+
         GSSManager manager = GSSManager.getInstance();
         GSSName gssServerName = manager.createName(serverName + "@" + host, GSSName.NT_HOSTBASED_SERVICE);
-        Oid krb5SpnegoOid = new Oid("1.3.6.1.5.5.2");
-        GSSName gssClientName = manager.createName(user, GSSName.NT_USER_NAME);
-        GSSCredential clientCreds = manager.createCredential(gssClientName, 8 * 3600, krb5SpnegoOid, GSSCredential.INITIATE_ONLY);
-        GSSContext secContext = manager.createContext(gssServerName, krb5SpnegoOid, clientCreds, GSSContext.DEFAULT_LIFETIME);
+        Oid desiredMech = new Oid("1.2.840.113554.1.2.2");
+        if (gssCredential == null) {
+            desiredMech = new Oid("1.3.6.1.5.5.2");     // SPNEGO
+            GSSName gssClientName = manager.createName(user, GSSName.NT_USER_NAME);
+            gssCredential = manager.createCredential(gssClientName, 8 * 3600, desiredMech, GSSCredential.INITIATE_ONLY);
+        }
+        GSSContext secContext = manager.createContext(gssServerName, desiredMech, gssCredential, GSSContext.DEFAULT_LIFETIME);
         secContext.requestMutualAuth(true);
         return Base64.encodeBase64String(secContext.initSecContext(new byte[0], 0, 0));
     }
