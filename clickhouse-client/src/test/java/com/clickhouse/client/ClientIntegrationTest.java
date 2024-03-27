8b3de24c6eb419e0d39e9d28e84f09f8e1804565
mzitnik <mark.zitnik@clickhouse.com>
2024-03-27 11:42:41 +0200

    build correctly the cloud endpoint

--- clickhouse-client/src/test/java/com/clickhouse/client/ClientIntegrationTest.java
+++ clickhouse-client/src/test/java/com/clickhouse/client/ClientIntegrationTest.java
@@ -207,6 +207,7 @@ public abstract class ClientIntegrationTest extends BaseIntegrationTest {
     }
 
     protected ClickHouseNode getServer() {
+        if (isCloud()) return getSecureServer(getProtocol());
         return getServer(getProtocol());
     }
 
