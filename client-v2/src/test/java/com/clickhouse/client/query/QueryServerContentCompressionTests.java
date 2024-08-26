6298b1e350300d191b06f7947037aff5ffb820db
Sergey Chernov <sergey.chernov@clickhouse.com>
2024-08-26 13:58:15 -0700

    fix compression by removing static buffer from the flow

--- client-v2/src/test/java/com/clickhouse/client/query/QueryServerContentCompressionTests.java
+++ client-v2/src/test/java/com/clickhouse/client/query/QueryServerContentCompressionTests.java
@@ -2,7 +2,7 @@ package com.clickhouse.client.query;
 
 public class QueryServerContentCompressionTests extends QueryTests {
     static {
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
+//        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
     }
     QueryServerContentCompressionTests() {
         super(true, false);
