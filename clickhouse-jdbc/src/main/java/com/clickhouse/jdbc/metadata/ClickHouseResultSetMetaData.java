87ffc509a94af0409f9772b6377c17d47c6dc7eb
Paultagoras <paul.moore@clickhouse.com>
2024-09-04 04:00:51 -0400

    Cleaning more existing code, adding some basic implementation from the old code

---
+++ clickhouse-jdbc/src/main/java/com/clickhouse/jdbc/metadata/ClickHouseResultSetMetaData.java
@@ -0,0 +1,114 @@
+package com.clickhouse.jdbc.metadata;
+
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import com.clickhouse.jdbc.JdbcWrapper;
+
+public class ClickHouseResultSetMetaData implements ResultSetMetaData, JdbcWrapper {
+
+    @Override
+    public int getColumnCount() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public boolean isAutoIncrement(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isCaseSensitive(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isSearchable(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isCurrency(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public int isNullable(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public boolean isSigned(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public int getColumnDisplaySize(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getColumnLabel(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public String getColumnName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public String getSchemaName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public int getPrecision(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int getScale(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getTableName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public String getCatalogName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public int getColumnType(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getColumnTypeName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public boolean isReadOnly(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isWritable(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isDefinitelyWritable(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public String getColumnClassName(int column) throws SQLException {
+        return "";
+    }
+}
