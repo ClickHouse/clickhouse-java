e36885f81fe1f02dc64cc2b0fe42f4546f344d2a
Sergey Chernov <sergey.chernov@clickhouse.com>
2024-11-21 10:14:08 -0800

    falled back to older collections API

--- client-v2/src/main/java/com/clickhouse/client/api/internal/EnvUtils.java
+++ client-v2/src/main/java/com/clickhouse/client/api/internal/EnvUtils.java
@@ -3,7 +3,9 @@ package com.clickhouse.client.api.internal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.util.Collections;
 
 /**
  * Environment utility class.
@@ -13,19 +15,26 @@ public class EnvUtils {
     private static final Logger LOG = LoggerFactory.getLogger(EnvUtils.class);
 
     /**
-     * Returns the local host name or address.
+     * Returns the local host name or IP address. Can be used to set {@code Referer} HTTP header.
+     * If fails to find the local host name or address, returns an empty string.
      * @param returnAddress if true, return address; otherwise, return name
      * @return string representing the local host name or address
      */
     public static String getLocalhostNameOrAddress(final boolean returnAddress) {
         try {
 
-            return NetworkInterface.networkInterfaces()
-                    .flatMap(NetworkInterface::inetAddresses)
-                    .filter(ia -> !ia.isLoopbackAddress())
-                    .findFirst()
-                    .map(ia -> returnAddress ? ia.getHostAddress() : ia.getCanonicalHostName())
-                    .orElse("");
+            for (NetworkInterface networkInterface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+                for (InetAddress inetAddress : Collections.list(networkInterface.getInetAddresses())) {
+                    if (inetAddress.isLoopbackAddress()) {
+                        continue;
+                    }
+                    if (returnAddress) {
+                        return inetAddress.getHostAddress();
+                    } else {
+                        return inetAddress.getCanonicalHostName();
+                    }
+                }
+            }
         } catch (Exception e) {
             LOG.error("Failed to get local host name or address", e);
         }
