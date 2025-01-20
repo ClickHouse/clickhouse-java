6e7d4ac8332794ef2ee69a629c50aae0eadcbe70
Paultagoras <paul.moore@clickhouse.com>
2025-01-20 03:12:01 -0500

    Update Basic.java

--- examples/jdbc/src/main/java/com/clickhouse/examples/jdbc/Basic.java
+++ examples/jdbc/src/main/java/com/clickhouse/examples/jdbc/Basic.java
@@ -27,12 +27,35 @@ public class Basic {
         properties.setProperty("user", System.getProperty("chUser", "default"));
         properties.setProperty("password", System.getProperty("chPassword", ""));
 
+        try {
+            createTable(url, properties);
+            insertDateWithPreparedStatement(url, properties);
+            printInsertedData(url, properties);
+
+            //Customizing client settings
+            setClientSettings(properties);
+
+            //Using HikariCP with ClickHouseDataSource
+            usedPooledConnection(url, properties);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void createTable(String url, Properties properties) throws SQLException {
         try (Connection conn = DriverManager.getConnection(url, properties)) {//Grab a connection using the jdbc DriverManager
             try (Statement stmt = conn.createStatement()) {//Create a statement
                 stmt.execute("DROP TABLE IF EXISTS " + TABLE_NAME);//Execute a query to drop the table if it exists
-                stmt.execute("CREATE TABLE " + TABLE_NAME + " (`date` Date, `id` UInt32, `name` String, `attributes` Map(String, String)) " +
-                        "ENGINE = MergeTree() ORDER BY id");//Create a table with three columns: date, id, and name
+                stmt.execute("CREATE TABLE " + TABLE_NAME +
+                        " (`date` Date, `id` UInt32, `name` String, `attributes` Map(String, String))" +
+                        " ENGINE = MergeTree() ORDER BY id");//Create a table with three columns: date, id, and name
+            }
+        }
+    }
 
+    static void insertDateWithPreparedStatement(String url, Properties properties) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(url, properties)) {//Grab a connection using the jdbc DriverManager
+            try (Statement stmt = conn.createStatement()) {
                 try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO " + TABLE_NAME + " VALUES(?, ?, ?, ?)")) {//Create a prepared statement
                     pstmt.setDate(1, Date.valueOf("2025-01-01"));//Set the first parameter to '2025-01-01' (using java.sql.Date)
                     pstmt.setInt(2, 1);//Set the second parameter to 1
@@ -48,32 +71,22 @@ public class Basic {
 
                     pstmt.executeBatch();//Execute the batch
                 }
+            }
+        }
+    }
 
+    static void printInsertedData(String url, Properties properties) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(url, properties)) {//Grab a connection using the jdbc DriverManager
+            try (Statement stmt = conn.createStatement()) {
                 try (ResultSet rs = stmt.executeQuery("SELECT * FROM " + TABLE_NAME)) {
                     while (rs.next()) {
                         System.out.println(rs.getDate(1) + ", " + rs.getInt(2) + ", " + rs.getString(3) + ", " + rs.getObject(4));
                     }
                 }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            //Using HikariCP with ClickHouseDataSource
-            usedPooledConnection(url, properties);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            //Customizing client settings
-            setClientSettings();
-        } catch (ClientException e) {
-            // Ignore
-            System.out.println(e.getMessage());
         }
     }
+
 
     static void usedPooledConnection(String url, Properties properties) throws SQLException {
         // connection pooling won't help much in terms of performance,
@@ -94,21 +107,16 @@ public class Basic {
         }
     }
 
-    static void setClientSettings() {
-        String url = System.getProperty("chUrl", "jdbc:ch://localhost:18123?jdbc_ignore_unsupported_values=true&socket_timeout=1");
-
-        // Set user and password if needed
-        Properties properties = new Properties();
-        properties.setProperty("user", System.getProperty("chUser", "default"));
-        properties.setProperty("password", System.getProperty("chPassword", ""));
+    static void setClientSettings(Properties properties){
+        String url = System.getProperty("chUrl", "jdbc:ch://localhost:18123?jdbc_ignore_unsupported_values=true&socket_timeout=10");
 
         try (Connection conn = DriverManager.getConnection(url, properties)) {
             try (Statement stmt = conn.createStatement()) {
-                try (ResultSet rs = stmt.executeQuery("SELECT sleep(3)")) {
-                    // this will throw an exception
-                    // because the query takes more than 1 second
-                    // and the socket timeout is set to 1 second
+                try (ResultSet rs = stmt.executeQuery("SELECT 1, 'Hello, world!'")) {
                     System.out.println(rs.next());
+                    System.out.println(rs.getInt(1));
+                    System.out.println(rs.getString(2));
+                    System.out.println(rs.getClob(2));//Unsupported type, but ignore_unsupported_values=true, so it won't throw an exception
                 }
             }
         } catch (SQLException e) {
