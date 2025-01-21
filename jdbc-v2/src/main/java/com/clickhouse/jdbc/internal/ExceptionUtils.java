0db21598bd2ec060f96505e4c7619e67b9b1cf18
Paultagoras <paul.moore@clickhouse.com>
2025-01-21 15:06:59 -0500

    Adjusting logging level of certain messages

--- jdbc-v2/src/main/java/com/clickhouse/jdbc/internal/ExceptionUtils.java
+++ jdbc-v2/src/main/java/com/clickhouse/jdbc/internal/ExceptionUtils.java
@@ -4,13 +4,18 @@ import com.clickhouse.client.api.ClientException;
 import com.clickhouse.client.api.ClientMisconfigurationException;
 import com.clickhouse.client.api.ConnectionInitiationException;
 import com.clickhouse.client.api.ServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.net.MalformedURLException;
+import java.sql.SQLDataException;
 import java.sql.SQLException;
 
 /**
  * Helper class for building {@link SQLException}.
  */
 public final class ExceptionUtils {
+    private static final Logger log = LoggerFactory.getLogger(ExceptionUtils.class);
     public static final String SQL_STATE_CLIENT_ERROR = "HY000";
     public static final String SQL_STATE_OPERATION_CANCELLED = "HY008";
     public static final String SQL_STATE_CONNECTION_EXCEPTION = "08000";
@@ -31,7 +36,7 @@ public final class ExceptionUtils {
      * @return Converted {@link SQLException}
      */
     public static SQLException toSqlState(Exception cause) {
-        return toSqlState( null, cause);
+        return toSqlState( null, null, cause);
     }
 
     /**
@@ -40,7 +45,9 @@ public final class ExceptionUtils {
      * @param cause {@link Exception} to convert
      * @return Converted {@link SQLException}
      */
-    public static SQLException toSqlState(String message, Exception cause) {
+    public static SQLException toSqlState(String message, String debugMessage, Exception cause) {
+        log.debug("Exception Message: {}, Debug message: {}", message, debugMessage, cause);
+
         if (cause == null) {
             return new SQLException(message == null ? "Unknown client error" : message, SQL_STATE_CLIENT_ERROR);
         }
@@ -57,6 +64,8 @@ public final class ExceptionUtils {
             return new SQLException(exceptionMessage, SQL_STATE_DATA_EXCEPTION, cause);
         } else if (cause instanceof ClientException) {
             return new SQLException(exceptionMessage, SQL_STATE_CLIENT_ERROR, cause);
+        } else if (cause instanceof MalformedURLException) {
+            return new SQLDataException(exceptionMessage, SQL_STATE_DATA_EXCEPTION, cause);
         }
 
         return new SQLException(exceptionMessage, SQL_STATE_CLIENT_ERROR, cause);//Default
