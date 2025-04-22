9484a423cc085e9f852978baf97ea017ee38de3d
mzitnik <mark@clickhouse.com>
2025-04-22 08:26:01 +0300

    Add testing for ALIAS and EPHEMERAL as well

--- client-v2/src/test/java/com/clickhouse/client/insert/SimplePOJO.java
+++ client-v2/src/test/java/com/clickhouse/client/insert/SimplePOJO.java
@@ -16,12 +16,14 @@ public class SimplePOJO {
     private static final Logger LOGGER = LoggerFactory.getLogger(SimplePOJO.class);
     private int int32;
     private String str;
+    private String hexed;
 
     public SimplePOJO() {
         long seed = System.currentTimeMillis();
         final Random random = new Random(seed);
         this.int32 = random.nextInt();
         this.str = RandomStringUtils.randomAlphabetic(1, 256);
+        this.hexed = RandomStringUtils.randomAlphanumeric(4);
     }
 
     public static String generateTableCreateSQL(String tableName) {
@@ -29,7 +31,11 @@ public class SimplePOJO {
                 "int32 Int32, " +
                 "str String, " +
                 "int64 Int64 MATERIALIZED abs(toInt64(int32)), " +
+                "str_lower String ALIAS lower(str), " +
+                "unhexed String EPHEMERAL, " +
+                "hexed FixedString(4) DEFAULT unhex(unhexed), " +
                 ") ENGINE = MergeTree ORDER BY ()";
     }
 
 }
+
