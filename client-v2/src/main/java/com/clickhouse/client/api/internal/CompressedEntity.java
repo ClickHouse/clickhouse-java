47067710a1b6de44584a4a17571a3677a8b8ca9a
Sergey Chernov <sergey.chernov@clickhouse.com>
2025-11-04 22:25:15 -0800

    added compression algorithms. fixed minor issues

---
+++ client-v2/src/main/java/com/clickhouse/client/api/internal/CompressedEntity.java
@@ -0,0 +1,100 @@
+package com.clickhouse.client.api.internal;
+
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.apache.hc.core5.function.Supplier;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpEntity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Set;
+
+public class CompressedEntity implements HttpEntity {
+
+    private HttpEntity httpEntity;
+    private final boolean isResponse;
+    private final CompressorStreamFactory compressorStreamFactory;
+    private final String contentEncoding;
+
+    CompressedEntity(HttpEntity httpEntity, boolean isResponse, CompressorStreamFactory compressorStreamFactory, String contentEncoding) {
+        this.httpEntity = httpEntity;
+        this.isResponse = isResponse;
+        this.compressorStreamFactory = compressorStreamFactory;
+        this.contentEncoding = contentEncoding;
+    }
+
+    @Override
+    public boolean isRepeatable() {
+        return httpEntity.isRepeatable();
+    }
+
+    @Override
+    public InputStream getContent() throws IOException, UnsupportedOperationException {
+        if (!isResponse) {
+            throw new UnsupportedOperationException("Unsupported: getting compressed content of request");
+        }
+
+        try {
+            return compressorStreamFactory.createCompressorInputStream(contentEncoding, httpEntity.getContent());
+        } catch (CompressorException e) {
+            throw new IOException("Failed to create decompressing input stream", e);
+        }
+    }
+
+    @Override
+    public void writeTo(OutputStream outStream) throws IOException {
+        if (isResponse) {
+            // called by us to get compressed response
+            throw new UnsupportedOperationException("Unsupported: writing compressed response to elsewhere");
+        }
+
+        try {
+            httpEntity.writeTo(compressorStreamFactory.createCompressorOutputStream(contentEncoding, outStream));
+        } catch (CompressorException e) {
+            throw new IOException("Failed to create compressing output stream", e);
+        }
+    }
+
+    @Override
+    public boolean isStreaming() {
+        return httpEntity.isStreaming();
+    }
+
+    @Override
+    public Supplier<List<? extends Header>> getTrailers() {
+        return httpEntity.getTrailers();
+    }
+
+    @Override
+    public void close() throws IOException {
+        httpEntity.close();
+    }
+
+    @Override
+    public long getContentLength() {
+        return httpEntity.getContentLength();
+    }
+
+    @Override
+    public String getContentType() {
+        return httpEntity.getContentType();
+    }
+
+    @Override
+    public String getContentEncoding() {
+        return httpEntity.getContentEncoding();
+    }
+
+    @Override
+    public boolean isChunked() {
+        return httpEntity.isChunked();
+    }
+
+    @Override
+    public Set<String> getTrailerNames() {
+        return httpEntity.getTrailerNames();
+    }
+}
--- client-v2/src/test/java/com/clickhouse/client/insert/InsertClientHttpCompressionTests.java
+++ client-v2/src/test/java/com/clickhouse/client/insert/InsertClientHttpCompressionTests.java
@@ -1,8 +1,70 @@
 package com.clickhouse.client.insert;
 
+import com.clickhouse.client.api.insert.InsertResponse;
+import com.clickhouse.client.api.insert.InsertSettings;
+import com.clickhouse.client.api.metrics.OperationMetrics;
+import com.clickhouse.client.api.query.GenericRecord;
+import com.clickhouse.data.ClickHouseFormat;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
 public class InsertClientHttpCompressionTests extends InsertTests {
 
     public InsertClientHttpCompressionTests() {
         super(true, true);
+    }
+
+
+    @Test(groups = { "integration" }, dataProvider = "insertRawDataCompressedProvider")
+    public void insertRawDataCompressed(String compressionAlgo) throws Exception {
+        final String tableName = "raw_data_table";
+        final String createSQL = "CREATE TABLE " + tableName +
+                " (Id UInt32, event_ts Timestamp, name String, p1 Int64, p2 String) ENGINE = MergeTree() ORDER BY ()";
+
+        initTable(tableName, createSQL);
+
+        InsertSettings insertSettings = InsertSettings.merge(settings, new InsertSettings());
+        insertSettings.setInputStreamCopyBufferSize(8198 * 2);
+        insertSettings.httpHeader(HttpHeaders.CONTENT_ENCODING, compressionAlgo);
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(data);
+        for (int i = 0; i < 1000; i++) {
+            writer.printf("%d\t%s\t%s\t%d\t%s\n", i, "2021-01-01 00:00:00", "name" + i, i, "p2");
+        }
+        writer.flush();
+        InsertResponse response = client.insert(tableName, new ByteArrayInputStream(data.toByteArray()),
+                ClickHouseFormat.TSV, insertSettings).get(30, TimeUnit.SECONDS);
+        OperationMetrics metrics = response.getMetrics();
+        assertEquals((int)response.getWrittenRows(), 1000 );
+
+        List<GenericRecord> records = client.queryAll("SELECT * FROM " + tableName);
+        assertEquals(records.size(), 1000);
+
+        for (int i = 0; i < records.size(); i++) {
+            assertEquals(records.get(i).getInteger(1), i);
+            assertEquals(records.get(i).getString("event_ts"), "2021-01-01 00:00:00");
+            assertEquals(records.get(i).getString("name"), "name" + i);
+            assertEquals(records.get(i).getInteger("p1"), i);
+            assertEquals(records.get(i).getString("p2"), "p2");
+        }
+    }
+
+    @DataProvider(name = "insertRawDataCompressedProvider")
+    public Object[][] insertRawDataCompressedProvider() {
+        return new Object[][] {
+            { "lz4" },
+            { "zstd" },
+            { "deflate" },
+            { "gz" },
+        };
     }
 }
