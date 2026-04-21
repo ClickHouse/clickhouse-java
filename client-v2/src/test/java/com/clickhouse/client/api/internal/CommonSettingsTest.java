3adaa91e20ca5c7774df95692b77376102bd59ab
Sergey Chernov <sergey.chernov@clickhouse.com>
2026-04-21 13:47:10 -0700

    Added tests and missing changelog records

---
+++ client-v2/src/test/java/com/clickhouse/client/api/internal/CommonSettingsTest.java
@@ -0,0 +1,21 @@
+package com.clickhouse.client.api.internal;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CommonSettingsTest {
+    @Test
+    public void testClearSession() {
+        CommonSettings settings = new CommonSettings();
+        settings.setSessionId("test-session");
+        settings.setSessionCheck(true);
+
+        Assert.assertEquals(settings.getSessionId(), "test-session");
+        Assert.assertEquals(settings.getSessionCheck(), Boolean.TRUE);
+
+        settings.clearSession();
+
+        Assert.assertNull(settings.getSessionId());
+        Assert.assertEquals(settings.getSessionCheck(), Boolean.FALSE);
+    }
+}
