8babe73e5f0818efe6a99032ea9907ae6aadfcdb
Sergey Chernov <sergey.chernov@clickhouse.com>
2026-06-22 15:05:31 -0700

    reworked tests

--- client-v2/src/test/java/com/clickhouse/client/api/ClientBuilderTest.java
+++ client-v2/src/test/java/com/clickhouse/client/api/ClientBuilderTest.java
@@ -2,6 +2,7 @@ package com.clickhouse.client.api;
 
 import com.clickhouse.client.api.enums.SSLMode;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Field;
@@ -23,6 +24,44 @@ public class ClientBuilderTest {
         }
     }
 
+    @Test(dataProvider = "testSslModeViaString_DP")
+    public void testSslModeViaString(String modeStr, SSLMode mode) {
+        try (Client client = new Client.Builder()
+                .addEndpoint("http://localhost:8123")
+                .setUsername("default")
+                .setPassword("")
+                .setOption(ClientConfigProperties.SSL_MODE.getKey(), modeStr)
+                .build()) {
+            Assert.assertEquals(client.getConfiguration().get(ClientConfigProperties.SSL_MODE.getKey()),
+                    mode.name());
+        }
+
+        try (Client client = new Client.Builder()
+                .addEndpoint("http://localhost:8123")
+                .setUsername("default")
+                .setPassword("")
+                .setSSLMode(mode)
+                .build()) {
+            Assert.assertEquals(client.getConfiguration().get(ClientConfigProperties.SSL_MODE.getKey()),
+                    mode.name());
+        }
+    }
+
+    @DataProvider
+    public static Object[][] testSslModeViaString_DP() {
+        return new Object[][] {
+                {"DISABLED", SSLMode.DISABLED},
+                {"disabled", SSLMode.DISABLED},
+                {"TRUST", SSLMode.TRUST},
+                {"trust", SSLMode.TRUST},
+                {"VERIFY_CA", SSLMode.VERIFY_CA},
+                {"verify_ca", SSLMode.VERIFY_CA},
+                {"STRICT", SSLMode.STRICT},
+                {"strict", SSLMode.STRICT},
+        };
+    }
+
+
     @Test
     public void testSslModeDisabledRejectedForHttpsRegardlessOfCase() {
         // Value supplied as a raw (non-canonical case) string via setOption must still be recognized
@@ -43,22 +82,8 @@ public class ClientBuilderTest {
                 .addEndpoint("https://localhost:8443")
                 .setUsername("default")
                 .setPassword("")
-                .setOption(ClientConfigProperties.SSL_MODE.getKey(), "insecure")
+                .setOption(ClientConfigProperties.SSL_MODE.getKey(), "verify-ca")
                 .build());
-    }
-
-    @Test
-    public void testSslModeNormalizedToCanonicalName() throws Exception {
-        // A non-canonical case value is accepted and normalized to the canonical enum name.
-        try (Client client = new Client.Builder()
-                .addEndpoint("http://localhost:8123")
-                .setUsername("default")
-                .setPassword("")
-                .setOption(ClientConfigProperties.SSL_MODE.getKey(), "trust")
-                .build()) {
-            Assert.assertEquals(client.getConfiguration().get(ClientConfigProperties.SSL_MODE.getKey()),
-                    SSLMode.TRUST.name());
-        }
     }
 
     private static String extractFirstEndpointUri(Client client) throws Exception {
